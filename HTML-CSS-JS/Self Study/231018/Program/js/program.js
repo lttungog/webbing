8db7e5d2a88e5130d6d89b84690b7aeaad215822
lttungog <124686851+lttungog@users.noreply.github.com>
2023-10-20 00:10:49 +0700

    SelfStudy 2

--- HTML-CSS-JS/Self Study/231018/Program/js/program.js
+++ HTML-CSS-JS/Self Study/231018/Program/js/program.js
@@ -28,6 +28,8 @@ var gtotal = total * percent;
 var elGTotal = document.getElementById('gtotal');
 elGTotal.textContent = gtotal;
 
+//----------------------------------------------------------------
+
 var product2 = 10;
 var price2 = 4;
 var total2 = price2 * product2;
@@ -57,3 +59,59 @@ var percent2 = (100 - discount2) / 100;
 var gtotal2 = total2 * percent2;
 var elGTotal2 = document.getElementById('gtotal2');
 elGTotal2.textContent = gtotal2;
+
+//--------------------------------------------------------------------
+
+function changeColor1() {
+    const contentElement = document.getElementById('text');
+
+    if (discount == '5') {
+        contentElement.style.color = 'green';
+    } else if (discount == '0'){
+        contentElement.style.color = 'red';
+    }
+}
+
+changeColor1();
+
+
+function changeColor2() {
+    const contentElement = document.getElementById('text2');
+
+    if (discount2 == '5') {
+        contentElement.style.color = 'green';
+    } else if (discount2 == '0'){
+        contentElement.style.color = 'red';
+    }
+}
+
+changeColor2();
+
+//-------------------------------------------------------------------------
+
+function updateTime() {
+    const timeElement = document.getElementById('time');
+    const currentTime = new Date();
+
+    const timeString = `Thời gian đầy đủ: ${currentTime}`;
+    timeElement.textContent = timeString;
+}
+
+setInterval(updateTime, 1000)
+
+updateTime();
+
+function changeTimeColor() {
+    const textElement = document.getElementById('time');
+    const currentColor = textElement.style.color;
+
+    if (currentColor === 'darkorchid') {
+        textElement.style.color = 'red';
+    } else if (currentColor === 'red') {
+        textElement.style.color = 'green';
+    } else {
+        textElement.style.color = 'blue';
+    }
+}
+
+setInterval(changeTimeColor, 360000)
